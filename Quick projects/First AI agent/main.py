3fa57de50910e5ed60bcf741a2af74537628adcc
Claudio Gonzalez <claudio.glez24@gmail.com>
2026-02-28 14:03:53 -0600

    initial structurefor the AI-development repo

---
+++ Quick projects/First AI agent/main.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+
+load_dotenv()
+
+#Define a Python class whioch will specigy the type of content that we want our LLM to generate
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    temperature=0
+)
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools = [])
+agent_executor = AgentExecutor(agent=agent, tools = [], verbose=True)
+
+raw_response = agent_executor.invoke({"query": "What is the impact of climate change on polar bears?"})
+#print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+except Exception as e:
+    print("Error parsing response:", e, "Raw response ->", raw_response)
+    structured_response = None  
+
+print(structured_response.tools_used)
---
+++ Quick projects/First AI agent/tools.py
@@ -0,0 +1,7 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
+search = DuckDuckGoSearchRun()
+search_tool = Tool()
